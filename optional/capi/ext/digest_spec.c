1e16253cecb0f8ee5292c5ea3805bf2cce2af97a
Herwin <herwinw@users.noreply.github.com>
2025-12-31 14:06:48 +0100

    FIXME: Add guard around PTR2NUM definition in digest_spec.c
    The upstream Ruby one has a smple #ifndef, but we can also match the
    version and get rid of the definition once 4.0 is the mimimum required
    version.

--- optional/capi/ext/digest_spec.c
+++ optional/capi/ext/digest_spec.c
@@ -135,7 +135,9 @@ VALUE digest_spec_context_size(VALUE self, VALUE meta) {
     return SIZET2NUM(algo->ctx_size);
 }
 
+#ifndef RUBY_VERSION_IS_4_0
 #define PTR2NUM(x) (rb_int2inum((intptr_t)(void *)(x)))
+#endif
 
 VALUE digest_spec_context(VALUE self, VALUE digest) {
     return PTR2NUM(context);
